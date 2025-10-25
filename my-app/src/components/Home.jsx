cda3036a631e4b9f4daabbe141a809da0aa05f08
sai charan <utukurisaicharan123@gmail.com>
2025-10-25 17:01:27 +0530

    Fixed resetpassword.css import path

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,5 +1,4 @@
-import {useState,useEffect} from "react";
-import { Routes,Route } from "react-router-dom";
+
 import Header from "./Header/Header";
 export default function Home(){
 return(
--- my-app/src/components/Resetpassword.jsx
+++ my-app/src/components/Resetpassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/ResetPassword.css";
+import "../styles/Resetpassword.css";
 import { toast } from "react-toastify";
 const API_URL_3000 = import.meta.env.VITE_API_URL_3000 || 'https://e-commerce-server-xezh.onrender.com ';
 

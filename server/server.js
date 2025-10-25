44cbff9ef65830515bc04293085aa0094e22cb80
sai charan <utukurisaicharan123@gmail.com>
2025-10-25 19:12:10 +0530

    Fixed bug in cart system

--- server/server.js
+++ server/server.js
@@ -58,8 +58,16 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error('CORS not allowed'), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const razorpay = new Razorpay({

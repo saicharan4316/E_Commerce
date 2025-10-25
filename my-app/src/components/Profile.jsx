e8340233e6f9f8f000de19d71c7b44f0a3f3617d
sai charan <utukurisaicharan123@gmail.com>
2025-10-25 17:03:33 +0530

    Fixed profile.css import path

--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Profile.css"
+import "../styles/profile.css"
  function ProfileView({ user }) {
   const navigate = useNavigate();
 
